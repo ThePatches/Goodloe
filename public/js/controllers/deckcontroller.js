94235d6d505ab87b6bdd35e1449d7d3d46487571
Patrick Taylor <warder05@gmail.com>
2014-02-17 09:32:12 -0500

    Replacing Ember with Angular (since Angular is better. :D)

---
+++ public/js/controllers/deckcontroller.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Patrick Taylor on 2/17/14.
+ */
+
+goodloeApp.controller('DeckController', function ($scope)
+{
+    $scope.Deck = {
+        name: "Rosheen",
+        color: "RG",
+        builder: "Chris M"
+    };
+});
