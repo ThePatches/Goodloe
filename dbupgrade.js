70c41afa1bd9d1e3c138d7a6f70ab85b5b5cf093
Patrick Taylor <warder05@gmail.com>
2014-04-29 22:02:07 -0400

    Adding to patch notes.

--- dbupgrade.js
+++ dbupgrade.js
@@ -6,6 +6,7 @@ var conn = new Mongo();
 var db = conn.getDB("goodloedb");
 
 var newPatches = ["Added Version Tracking from the Database",
-    "You can now set commanders independently of deck name."];
+    "You can now set commanders independently of deck name.",
+    "Moved messaging from notification service to email service"];
 
 db.Version.insert({_id: 1, version: "0.5.1", patches: newPatches, published: false});
