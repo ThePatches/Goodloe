8435a984d7f09df4aefadf9e13ef39e4ed4e7da8
Patrick Taylor <warder05@gmail.com>
2014-04-29 22:10:01 -0400

    patching dbupgrade.js to be correct before merging into deployment.

--- dbupgrade.js
+++ dbupgrade.js
@@ -9,4 +9,4 @@ var newPatches = ["Added Version Tracking from the Database",
     "You can now set commanders independently of deck name.",
     "Moved messaging from notification service to email service"];
 
-db.Version.insert({_id: 1, version: "0.5.1", patches: newPatches, published: false});
+db.Version.insert({_id: 1, version: "0.5.1", changes: newPatches, published: false});
