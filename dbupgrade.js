584ac0d0d9a6a3f7cca96b67f13628209c8ccee4
Patrick Taylor <warder05@gmail.com>
2014-05-03 15:32:16 -0400

    some stylistic fixes

--- dbupgrade.js
+++ dbupgrade.js
@@ -4,6 +4,6 @@
 
 var newPatches = ["User Updating feature added",
     "Fixed issues with + signs within the database",
-    "Beginnings of 'Goodoe Rules' page"];
+    "Beginnings of 'Goodloe Rules' page"];
 
 db.Version.insert({_id: 2, version: "0.6.1", patches: newPatches, published: false});
