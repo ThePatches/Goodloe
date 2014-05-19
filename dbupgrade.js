887af506bad73b014c9ae7e32a75a4146f861cb8
Patrick Taylor <warder05@gmail.com>
2014-05-18 23:03:57 -0400

    Added win percentage to each player's page.

--- dbupgrade.js
+++ dbupgrade.js
@@ -5,6 +5,6 @@
 //var conn = new Mongo();
 //var db = conn.getDB("goodloedb");
 
-var newPatches = ["Added a User List for me", "Fixed an issue with updating decks when there was no list provided."];
+var newPatches = ["Added a User List for me", "Fixed an issue with updating decks when there was no list provided.", "Added a win percentage to the player page."];
 
 db.Version.insert({_id: 5, version: "0.6.4", changes: newPatches, published: false});
