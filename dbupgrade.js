145bb27ef17c4cdeb58183852df75beb8e64a0c5
Patrick Taylor <warder05@gmail.com>
2014-05-25 09:38:44 -0400

    Made the player win percentages look reasonable.

--- dbupgrade.js
+++ dbupgrade.js
@@ -5,6 +5,7 @@
 //var conn = new Mongo();
 //var db = conn.getDB("goodloedb");
 
-var newPatches = ["Moved to Express4", "Reworked the server to make more sense", "Added Select2 on the Game Page for deck selection", "Reworked the layout of the Game page"];
+var newPatches = ["Moved to Express4", "Reworked the server to make more sense", "Added Select2 on the Game Page for deck selection",
+    "Reworked the layout of the Game page", "Added a filter for the Deck list", "Made the player percentage display sane"];
 
 db.Version.insert({_id: 6, version: "1.0.1", changes: newPatches, published: false});
