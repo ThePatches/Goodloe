e637a3f692715eb6bc0f73256336bd50f2d258fc
Patrick Taylor <warder05@gmail.com>
2014-05-26 08:54:47 -0400

    Moved the global directives into their own file.

---
+++ public/js/directives/globaldirectives.js
@@ -0,0 +1,66 @@
+/**
+ * Created by Patrick Taylor on 5/26/14.
+ */
+
+angular.module("GlobalDirectives", [])
+    .directive("clearInputs", function()
+    {
+        return {
+            restrict: "A",
+            controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs)
+            {
+                var clearDivId = $attrs.formDiv;
+                $element.click(function()
+                {
+                    $("#" + clearDivId + "> input").each(function ()
+                    {
+                        $(this).val("");
+                    });
+
+                    $("#" + clearDivId + ">textarea").each(function ()
+                    {
+                        $(this).val("");
+                    });
+                });
+            }]
+        };
+    })
+
+    .directive("patchExpander", function()
+    {
+        return {
+            restrict: "A",
+            controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs)
+            {
+                var parentElement = $element.parent();
+                var list = parentElement.find("ul");
+
+                $element.click(function ()
+                {
+                    if (list.is(":visible"))
+                        list.css("display", "none");
+                    else
+                        list.css("display", "block");
+                });
+
+                $element.ready(function ()
+                {
+                    if ($attrs.patchIndex > 0)
+                        list.css("display", "none");
+                });
+            }]
+        };
+    })
+    .directive("navBlock", function()
+    {
+        return {
+            restrict: "E",
+            templateUrl: "navblock.html",
+            controller: ['$scope', '$element', '$attrs', '$location', function($scope, $element, $attrs, $location)
+            {
+                alert("not implemented!");
+            }]
+        };
+    })
+
+;
