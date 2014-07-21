b4e04c78f299048d612aacc8cbe085472a98b18b
Patrick Taylor <warder05@gmail.com>
2014-07-20 22:43:42 -0400

    correct number for update log, final format change (changlog page)

--- dbupgrade.js
+++ dbupgrade.js
@@ -2,12 +2,12 @@
  * Created by Patrick Taylor on 4/28/14.
  */
 
-//var conn = new Mongo();
-//var db = conn.getDB("goodloedb");
+var conn = new Mongo();
+var db = conn.getDB("goodloedb");
 
 var newPatches = ["Added card banning functionality", "Added TypeKit and did some prettifying of the interface", "Decks can be deactivated."];
 
-db.Version.insert({_id: 9, version: "1.2.0", changes: newPatches, published: false});
+db.Version.insert({_id: 8, version: "1.2.0", changes: newPatches, published: false});
 
 db.Deck.update({}, {$set: {isActive: true}}, {multi: true});
 db.GoodUsers.update({name: "Chris"}, {$set: {adminRights: 2}}, {multi: false});
